6a5965f6e75c6ad0fa7911b04369731ed11d1a79
liaolinyan2018 <linyanliao@126.com>
2018-12-28 11:36:59 +0800

    修改为付款 新增数据库db

---
+++ server.js
@@ -0,0 +1,57 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('HTTP 路径为\n' + path)
+  if(path === '/'){
+    var string = fs.readFileSync('./index.html','utf8')
+    var amount = fs.readFileSync('./db','utf8')//100
+    string = string.replace('&&&amount&&&',amount) //用amount内容代替&&&amount&&&
+    response.setHeader('Content-Type','text/html')
+    response.write(string)
+    response.end()
+  }else if(path === '/style.css'){
+    var string = fs.readFileSync('./style.css','utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){
+    var string = fs.readFileSync('./main.js','utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else if(path === '/pay' && method.toUpperCase() === 'POST'){
+    var amount = fs.readFileSync('./db','utf8') //100
+    var newAmount = amount - 1
+    fs.writeFileSync('./db',newAmount);
+    response.write('success')
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    response.write('找不到对应的路径，你需要自行修改server.js')
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
